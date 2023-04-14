ce1614a1f80623e48500f312d82a7354d11132d4
Foxy20122012 <enriquemerlos49@gmail.com>
2023-04-14 01:56:48 -0600

    se agreron los estilos

--- Logica.js
+++ Logica.js
@@ -1,1 +1,8 @@
 // document.write("Se enlajo JavaScript");
+
+const Encrintando =()=>{
+    const frase = document.getElementById("frase").value;
+
+    const fraseCriptada = frase
+                                .replace(/e/gi,"enter")
+}
