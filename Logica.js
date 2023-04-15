5398c20bad9d9b61f5d47ebb088a61d489e0ce69
Foxy20122012 <enriquemerlos49@gmail.com>
2023-04-14 19:03:22 -0600

    Funciones de Encriptado y Desencriptado

--- Logica.js
+++ Logica.js
@@ -12,13 +12,37 @@ const Encrintando =()=>{
         .replace(/a/gi, "ai")
         .replace(/o/gi, "ober")
         .replace(/u/gi, "ufat");
-    if (frase.length !=0) {
-        frase = fraseCriptada;
-        titulo.textContent = "Texto encriptado con exito";
-        parrafo.textContent = "";
-        muñeco.src = "https://png.pngtree.com/png-clipart/20190904/original/pngtree-thinking-simple-characters-free-elements-png-image_4481227.jpg";
+    if (document.getElementById("frase").value.length != 0) {
+        document.getElementById("frase").value = fraseCriptada;
+        document.getElementById("titulo").value = titulo;
+        document.getElementById("parrafo").value = parrafo;
+        document.getElementById("muñeco").src = "https://d22k5h68hofcrd.cloudfront.net/magefan_blog/Como_cifrar_correos_y_enviar_mensajes_seguros.jpg";
     }else{
-        muñeco.src= "img/Muñeco (1).png";
+        document.getElementById("muñeco").src= "img/Muñeco (1).png";
+        alert("Debes ingresar algún texto");
+    }
+
+}
+
+const Desencriptar =()=>{
+    const frase = document.getElementById("frase").value;
+    const titulo = document.getElementById("titulo");
+    const parrafo = document.getElementById("parrafo");
+    const muñeco =  document.getElementById("muñeco");
+
+    const fraseDesCriptada = frase
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+    if (document.getElementById("frase").value.length != 0) {
+        document.getElementById("frase").value = fraseDesCriptada;
+        document.getElementById("titulo").value = titulo;
+        document.getElementById("parrafo").value = parrafo;
+        document.getElementById("muñeco").src = "https://elceo.com/wp-content/uploads/2020/11/smartphone.jpg";
+    }else{
+        document.getElementById("muñeco").src= "img/Muñeco (1).png";
         alert("Debes ingresar algún texto");
     }
 
